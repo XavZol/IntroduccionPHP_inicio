b7b55c0256b1dba0a5dd2439bd37a5aa16493c8e
XavZol <alansolis707@gmail.com>
2025-11-15 02:32:21 -0600

    Fizz Buzz

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,41 @@
 <?php include 'includes/header.php';
 
+// While
+$i = 100;
 
+while ($i < 0) {
+
+    echo $i . "<br>";
+
+    $i++; // incremento
+}
+
+echo "<br>";
+// Do while
+$i = 100;
+
+do {
+    echo $i . "<br>";
+
+    $i++;
+} while ($i < 10);
+
+/**
+ * 3 imprimir Fizz
+ * 5 Imprimir Buzz
+ * 3 y 5 imprimir Fizz Buzz
+ */
+
+// for loop.
+for ($i = 1; $i < 1000; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo $i . " - FIZZ BUZZ <br/>";
+    } else if ($i % 3 === 0) {
+        echo $i . " - Fizz <br/>";
+    } else if ($i % 5 === 0) {
+        echo $i . " - Buzz <br/>";
+    }
+}
 
 
 include 'includes/footer.php';
